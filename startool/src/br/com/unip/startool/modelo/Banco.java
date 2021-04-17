e26c7b500be7ce928875d221ed273cf09b27c1a1
Erik Santos <erik311009r@gmail.com>
2021-04-16 23:13:19 -0300

    Criando os dados do Banco

--- startool/src/br/com/unip/startool/modelo/Banco.java
+++ startool/src/br/com/unip/startool/modelo/Banco.java
@@ -1,5 +1,23 @@
 package br.com.unip.startool.modelo;
 
-public class Banco {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Banco {
+	private static List<Startups> listaStartup = new ArrayList<Startups>();
+
+	static {
+		Startups s1 = new Startups();
+		Startups s2 = new Startups();
+		Startups s3 = new Startups();
+		Startups s4 = new Startups();
+		Startups s5 = new Startups();
+	}
+	public List<Startups> getListaStartup() {
+		return listaStartup;
+	}
+
+	public void adicionar(Startups startup) {
+		listaStartup.add(startup);
+	}
 }
