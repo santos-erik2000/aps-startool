cc0c2b9008490fe509e516cee641df3fb7f7ff02
SarahTeodoro <71534242+SarahTeodoro@users.noreply.github.com>
2021-04-17 00:01:57 -0300

    Criado na entidade Startup, os Atributos (Nome, Projeto, Unidade,  Ferramenta, Descrição).

---
+++ startool/src/br/com/unip/startool/modelo/Descricao.java
@@ -0,0 +1,26 @@
+package br.com.unip.startool.modelo;
+
+public class Descricao {
+	
+	private String titulo;
+	private String assunto;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	
+
+	
+
+}
--- startool/src/br/com/unip/startool/modelo/Startups.java
+++ startool/src/br/com/unip/startool/modelo/Startups.java
@@ -1,5 +1,46 @@
 package br.com.unip.startool.modelo;
 
-public class Startups {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Startups {
+	
+	private String nome;
+	private String projeto; 
+	private String unidade;
+	private List <String> ferramentas = new ArrayList<>();
+	private Descricao descricao;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getProjeto() {
+		return projeto;
+	}
+	public void setProjeto(String projeto) {
+		this.projeto = projeto;
+	}
+	public String getUnidade() {
+		return unidade;
+	}
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+	public List<String> getFerramentas() {
+		return ferramentas;
+	}
+	public void setFerramentas(List<String> ferramentas) {
+		this.ferramentas = ferramentas;
+	}
+	public Descricao getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(Descricao descricao) {
+		this.descricao = descricao;
+	}
+	
 }
