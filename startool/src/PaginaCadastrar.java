b9106af092461ffaa6f3b798765a06b4b84d482c
SarahTeodoro <sarahb.teodoro@gmail.com>
2021-04-27 21:37:45 -0300

    Criado classe "PaginaCadastrar"e a "PaginaLogin" e feito o return"foward"pagina-login

--- startool/src/PaginaCadastrar.java
+++ startool/src/PaginaCadastrar.java
@@ -1,13 +1,10 @@
-package br.com.unip.startool.acao;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PaginaLogin {
-
+public class PaginaCadastrar {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		
-		return "forward:pagina-login.jsp";
+		return "forward:pagina-cadastro.jsp";
 		
 	}
 
