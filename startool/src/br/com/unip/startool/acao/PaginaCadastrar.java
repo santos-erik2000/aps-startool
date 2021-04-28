5f5438c9d8373f92f25ea6163cba3bf98c3d1164
SarahTeodoro <sarahb.teodoro@gmail.com>
2021-04-27 22:01:35 -0300

    Foi alterado o caminho da a "PaginaCadastrar".

--- startool/src/br/com/unip/startool/acao/PaginaCadastrar.java
+++ startool/src/br/com/unip/startool/acao/PaginaCadastrar.java
@@ -1,10 +1,13 @@
+package br.com.unip.startool.acao;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class PaginaCadastrar {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		
+		System.out.println("Calcinha pretra");
 		return "forward:pagina-cadastro.jsp";
+		
 		
 	}
 
