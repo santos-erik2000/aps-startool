3d7230581aa0635d551a9aa2049ab0efb80af037
Lucas <lucasoliveiraahh@gmail.com>
2021-05-21 15:58:09 -0300

    Script ferramentas

--- startool/WebContent/assets/js/script.js
+++ startool/WebContent/assets/js/script.js
@@ -136,3 +136,10 @@
                 }
 	
 	
+	
+	var capturando = "";
+function capturar () {
+    capturando = document.getElementById('valor').value;
+    document.getElementById('valorDigitado').innerHTML = capturando;
+}
+	
